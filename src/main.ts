f4720e3578f2b0e4cca90cee463964d8f415a870
dnatuna <dnatuna123@gmail.com>
2021-08-31 16:45:34 +0900

    Add: Prisma module 세팅 완료

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
+  app.useGlobalPipes(new ValidationPipe());
   const nestConfig = app.get(ConfigService);
-
   await app.listen(nestConfig.get('port'), nestConfig.get('host'));
 }
 
