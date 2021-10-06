defe37ec69b5ced0a167108c523b4bf41a9efd63
dnatuna <dnatuna123@gmail.com>
2021-10-07 00:18:34 +0900

    Fix: JwtAuthGuard를 JwtAccessAuthGuard로 변경

--- src/running/controllers/single-running.controller.ts
+++ src/running/controllers/single-running.controller.ts
@@ -7,7 +7,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { DeserializeAccessToken } from 'src/auth/dto/auth.dto';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { JwtAccessAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { User } from 'src/user/decorators/user.decorator';
 import { RunningRequest } from '../dto/running.dto';
 import {
@@ -20,7 +20,7 @@ import { SingleRunningService } from '../services/single-running.service';
 export class SingleRunningController {
   constructor(private readonly singleRunningService: SingleRunningService) {}
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAccessAuthGuard)
   @Post()
   runStart(
     @User() user: DeserializeAccessToken,
@@ -29,13 +29,13 @@ export class SingleRunningController {
     return this.singleRunningService.runStart(user, body);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAccessAuthGuard)
   @Post('running')
   running(@Body() body: RunningRequest): Promise<SingleRunningResponse> {
     return this.singleRunningService.running(body);
   }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAccessAuthGuard)
   @Patch(':id')
   runEnd(@Param('id') id: string): Promise<SingleRunningResponse> {
     return this.singleRunningService.runEnd(id);
