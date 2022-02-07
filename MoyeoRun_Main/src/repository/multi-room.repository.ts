ed85d29e6f031d23a8ff1a93a8455bc9f4c1ab79
GeonHun Kim <dnatuna123@gmail.com>
2022-02-08 01:01:40 +0900

    Update MoyeoRun_Main/src/repository/multi-room.repository.ts

--- MoyeoRun_Main/src/repository/multi-room.repository.ts
+++ MoyeoRun_Main/src/repository/multi-room.repository.ts
@@ -115,7 +115,7 @@ export class MultiRoomRepository {
       include: {
         multiRoomMember: {
           include: {
-            multiRoomUser: false,
+            multiRoomUser: true,
           },
         },
       },
